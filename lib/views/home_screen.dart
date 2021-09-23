9429ddc6cad243a271402de75aff2a4946e4b9b5
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-09-23 18:59:52 +0600

    AppBar done

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../size_config.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // We have to call this on our starting page
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/Settings.svg",
+          color: Theme.of(context).iconTheme.color,
+        ),
+        onPressed: () {},
+      ),
+      actions: [buildAddButton(context)],
+    );
+  }
+
+  Padding buildAddButton(BuildContext context) {
+    return Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(10),
+            vertical: getProportionateScreenHeight(10)
+          ),
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              width: getProportionateScreenWidth(32),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        );
+  }
+}
