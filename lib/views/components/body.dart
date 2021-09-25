4d2cf14602da4391cfdef4b40ba36537073101f8
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-09-25 18:48:04 +0600

    country card completed

--- lib/views/components/body.dart
+++ lib/views/components/body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:light_dark_theme/size_config.dart';
 import 'package:light_dark_theme/views/components/clock.dart';
+import 'country_card.dart';
 import 'time_in_hour_and_minute.dart';
 
 class Body extends StatelessWidget {
@@ -7,19 +10,44 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        children: <Widget>[
-          Text(
-            "Newport Beach, USA | PST",
-            style: Theme.of(context).textTheme.bodyText1,
-          ),
-          const TimeInHourAndMinute(),
-          const Clock(),
-        ],
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Dhaka, BD | PST",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const TimeInHourAndMinute(),
+            const Spacer(),
+            const Clock(),
+            const Spacer(),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: const [
+                  CountryCard(
+                    country: "New York, USA",
+                    timeZone: "+3 HRS | EST",
+                    iconSrc: "assets/icons/Liberty.svg",
+                    period: "PM",
+                    time: "9:20",
+                  ),
+                  CountryCard(
+                    country: "Sydney, AU",
+                    timeZone: "+19 HRS | AEST",
+                    iconSrc: "assets/icons/Sydney.svg",
+                    period: "AM",
+                    time: "1:20",
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
 }
-
